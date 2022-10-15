5c5cbf1b2a6a33d3a6088c168050488e37dc370a
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-10-15 14:41:44 +0200

    Add a quackspam message checker

---
+++ checkers/quackspam.js
@@ -0,0 +1,25 @@
+const { Message } = require('discord.js');
+
+const config = require('../config.json');
+
+/**
+ *
+ * @param {Message} message The message to check
+ */
+module.exports.check = (message) => {
+	if (!message?.deletable || message?.member?.permissionsIn(message?.channel).has('MANAGE_GUILD')) return false;
+	const message_content = message?.content?.toLowerCase();
+	if (message?.author?.bot && !message_content.includes(config.messages_patters.level_up)) {
+		message.delete().catch(() => {});
+		return true;
+	}
+	if (
+		!message_content.includes(config.messages_patters.level_up) &&
+		message_content != 'quackquack' &&
+		message_content != 'quack' &&
+		message_content != 'quack quack'
+	) {
+		message.delete().catch(() => {});
+		return true;
+	}
+};
